21e6599e29117c2649a292e152829686e9bf0631
x183 <palmoscar4@gmail.com>
2022-08-31 09:11:18 +0200

    Added some nice assets

---
+++ assets/tilesets/forest/forest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="forest" tilewidth="32" tileheight="32" tilecount="126" columns="7">
+ <image source="forest_.png" trans="ff00ff" width="240" height="576"/>
+</tileset>
