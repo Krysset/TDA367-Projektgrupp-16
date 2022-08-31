8f3b34ba1afcb2012f00d5149091f772525f658c
x183 <palmoscar4@gmail.com>
2022-08-31 11:35:19 +0200

    Added health functionality to BaseCreature

--- core/src/com/g16/feyrune/creature/BaseCreature.java
+++ core/src/com/g16/feyrune/creature/BaseCreature.java
@@ -2,8 +2,11 @@ package com.g16.feyrune.creature;
 
 import com.badlogic.gdx.math.Vector2;
 
+import static java.lang.Math.min;
+
 public class BaseCreature {
     //Stats variables
+    private double maxHealth;
     private double health;
     private double damage;
     private double movementSpeed;
@@ -12,20 +15,29 @@ public class BaseCreature {
     private Vector2 position;
 
     public BaseCreature(double health, double damage, double movementSpeed, boolean aggressive, Vector2 position){
-        this.health = health;
+        this.maxHealth = health;
         this.damage = damage;
         this.movementSpeed = movementSpeed;
         this.aggressive = aggressive;
         this.position = position;
+        this.health = this.maxHealth;
     }
 
     public void spawn(){
 
     }
-    public void die(){
+    private void die(){
 
     }
     public void attack(BaseCreature enemy){
 
+    }
+
+    public void changeHealth(double deltaHealth){
+        double newHealth = health + deltaHealth;
+        if (newHealth <= 0.0d){
+            die();
+        }
+        health = min(newHealth, maxHealth);
     }
 }
