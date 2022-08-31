ac7a851e88e5fb5f6aa7a43b9f6d2d1be0ab952f
x183 <palmoscar4@gmail.com>
2022-08-31 16:03:44 +0200

    created a class to handle all attacks

--- core/src/com/g16/feyrune/action/Attack.java
+++ core/src/com/g16/feyrune/action/Attack.java
@@ -8,24 +8,20 @@ public class Attack {
     private int accuracy;
     private int power;
 
-    private Random randomCalc;
 
     public Attack(int accuracy, int power){
         this.accuracy = accuracy;
         this.power = power;
-        randomCalc = new Random();
     }
     private double calculateDamage(double attack){
         double trueDamage=max(0, attack*power);
         return trueDamage;
     }
 
-    public double tryAttack(double attack){
-        double trueDamage=calculateDamage(attack);
-        int r=randomCalc.nextInt(0,100);
-        if ( r < accuracy){
-            return trueDamage;
-        }
-        return 0;
+    public int getPower(){
+        return power;
+    }
+    public int getAccuracy(){
+        return accuracy;
     }
 }
---
+++ core/src/com/g16/feyrune/model/combatHandler.java
@@ -0,0 +1,48 @@
+package com.g16.feyrune.model;
+
+import com.g16.feyrune.action.Attack;
+import com.g16.feyrune.creature.BaseCreature;
+
+import java.util.Random;
+
+public class combatHandler {
+
+    private static final Random randomCheck=new Random();
+
+    /**
+     * Used to make a monster attack another monster
+     * @param attacker The monster performing the attack
+     * @param defender The monster trying to evade the attack
+     * @param attack The specific attack being used
+     */
+    public static void handleAttack(BaseCreature attacker, BaseCreature defender, Attack attack){
+        if (evasiveManoeuvre(defender, attack)){
+            double damage = calculateDamage(attacker, attack);
+            defender.damageMonster(damage);
+        }
+    }
+
+    /**
+     *
+     * @param attacker: creature containing the attack used in the action
+     * @param attack: the attack action used against the defender
+     * @return damage dealt to the defender (only calculates, doesn't deal it)
+     */
+    private static double calculateDamage(BaseCreature attacker, Attack attack){
+        double damage = attacker.getAttack() * attack.getPower();
+        return damage;
+    }
+
+    /**
+     * Calculates whether the attack accuracy is high enough to hit and whether the defender's evasiveness is high enough to evade it
+     * @param defender: monster trying to evade the attack
+     * @param attack: attack to check accuracy on.
+     * @return if attack is successful: true, if it misses false
+     */
+    private static boolean evasiveManoeuvre(BaseCreature defender, Attack attack){
+        boolean evasion = defender.getEvasion()<randomCheck.nextInt(0,100);
+        boolean accuracy = attack.getAccuracy()>randomCheck.nextInt(0,100);
+        boolean hit= evasion && accuracy;
+        return hit;
+    }
+}
