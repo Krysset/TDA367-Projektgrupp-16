b9bf1d0474ab02224fb88994d099b9cad077428d
x183 <palmoscar4@gmail.com>
2022-08-31 14:36:44 +0200

    Added evade functionality

--- core/src/com/g16/feyrune/creature/BaseCreature.java
+++ core/src/com/g16/feyrune/creature/BaseCreature.java
@@ -2,6 +2,8 @@ package com.g16.feyrune.creature;
 
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.Random;
+
 import static java.lang.Math.min;
 
 public class BaseCreature {
@@ -11,13 +13,17 @@ public class BaseCreature {
     private double attack;
     private double movementSpeed;
     private Vector2 position;
+    private int evasion;
+    private Random randomCalc;
 
-    public BaseCreature(double health, double attack, double movementSpeed, Vector2 position){
+    public BaseCreature(double health, double attack, double movementSpeed, Vector2 position, int evasion){
         this.maxHealth = health;
         this.attack = attack;
         this.movementSpeed = movementSpeed;
         this.position = position;
         this.health = this.maxHealth;
+        this.evasion = evasion;
+        randomCalc = new Random();
     }
 
     public void spawn(){
@@ -30,11 +36,29 @@ public class BaseCreature {
 
     }
 
-    public void changeHealth(double deltaHealth){
-        double newHealth = health + deltaHealth;
+    private void changeHealth(double healthChange){
+        double newHealth = health + healthChange;
         if (newHealth <= 0.0d){
             die();
         }
         health = min(newHealth, maxHealth);
+    }
+
+    private boolean evade(){
+        int tryEvade = randomCalc.nextInt(0,100);
+        return (tryEvade < evasion)? true : false;
+    }
+    public void hurt(double damage){
+        if(!evade()){
+            changeHealth(-damage);
+        }
+    }
+
+    /**
+     * Adds health to the creature
+     * @param: Amount to heal (though maxHealth is max total)
+     */
+    public void heal(double addedHealth){
+        changeHealth(addedHealth);
     }
 }
