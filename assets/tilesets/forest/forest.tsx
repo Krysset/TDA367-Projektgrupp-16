7f1192c2c79693486c3c9b867663ebeba54f9188
x183 <palmoscar4@gmail.com>
2022-08-31 09:37:57 +0200

    added a third test map

--- assets/tilesets/forest/forest.tsx
+++ assets/tilesets/forest/forest.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.1" name="forest" tilewidth="32" tileheight="32" tilecount="126" columns="7">
+<tileset version="1.9" tiledversion="1.9.1" name="forest" tilewidth="16" tileheight="16" tilecount="540" columns="15">
  <image source="forest_.png" trans="ff00ff" width="240" height="576"/>
 </tileset>
