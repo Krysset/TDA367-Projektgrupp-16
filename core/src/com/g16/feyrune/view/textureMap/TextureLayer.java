3fb8fb2c1b433c17f80f9f5851db533c45a9e370
Krysset <maxhagman@hotmail.se>
2022-09-02 15:11:00 +0200

    Created basic TextureLayer

---
+++ core/src/com/g16/feyrune/view/textureMap/TextureLayer.java
@@ -0,0 +1,32 @@
+package com.g16.feyrune.view.textureMap;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class TextureLayer {
+    private TextureTile[][] textureTileMap;
+    private String name;
+    private int mapWidth, mapHeight, tileWidth, tileHeight;
+    private float layer;
+
+    public TextureLayer(String name, int mapWidth, int mapHeight, int tileWidth, int tileHeight, float layer) {
+        this.mapWidth = mapWidth;
+        this.mapHeight = mapHeight;
+        this.tileWidth = tileWidth;
+        this.tileHeight = tileHeight;
+        this.layer = layer;
+        this.name = name;
+        textureTileMap = new TextureTile[mapWidth][mapHeight];
+    }
+
+    public void draw(SpriteBatch batch) {
+        for (int x = 0; x < mapWidth; x++) {
+            for (int y = 0; y < mapHeight; y++) {
+                // TODO: draw tiles with batch
+            }
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
