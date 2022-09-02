e2aea023adb5a7e2b65ae3fed07a131fc99c9cc5
x183 <palmoscar4@gmail.com>
2022-09-02 14:54:12 +0200

    Removed default button

---
+++ core/src/com/g16/feyrune/view/scenes/BattleScene.java
@@ -0,0 +1,27 @@
+package com.g16.feyrune.view.scenes;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.g16.feyrune.interfaces.Observer;
+
+public class BattleScene extends Scene{
+
+    private Observer[]
+
+    public BattleScene(int viewWidth, int viewHeight) {
+        super(viewWidth, viewHeight, new Stage(), new Stage(), new Stage());
+        createuiStage();
+        createobjectStage();
+        createterrainStage();
+    }
+
+    private void createuiStage(){
+
+
+    }
+    private void createobjectStage(){
+
+    }
+    private void createterrainStage(){
+
+    }
+}
