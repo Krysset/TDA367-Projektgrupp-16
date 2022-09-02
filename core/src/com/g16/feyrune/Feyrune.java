05e5af66c15a52ced911c89b2ee0d3273ec334d1
x183 <palmoscar4@gmail.com>
2022-09-02 16:16:06 +0200

    removed testing from feyrune

--- core/src/com/g16/feyrune/Feyrune.java
+++ core/src/com/g16/feyrune/Feyrune.java
@@ -10,34 +10,21 @@ import com.g16.feyrune.view.scenes.BattleScene;
 public class Feyrune extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	BattleScene battleScene;
 
-	public class Helu implements Observer {
-		public Helu(){
-			super();
-		}
-		@Override
-		public void update() {
-			System.out.println("helu");
-		}
-	}
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		Helu[] h ={new Helu(), new Helu()};
-		battleScene = new BattleScene(800, 600, h);
 
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
-		battleScene.render(25);
-		//batch.begin();
-		//batch.draw(img, 0, 0);
-		//batch.end();
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
 	}
 	
 	@Override
