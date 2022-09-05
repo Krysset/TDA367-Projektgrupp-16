a17c78872dbc7b583f552c01a015408ed05fc97a
Krysset <maxhagman@hotmail.se>
2022-09-05 10:35:35 +0200

    Tileset parsing added

---
+++ core/src/com/g16/feyrune/view/textureMap/TextureMapParser.java
@@ -0,0 +1,101 @@
+package com.g16.feyrune.view.textureMap;
+
+import com.sun.tools.javac.util.Pair;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TextureMapParser {
+    public static TextureMap parseMapFile(String filePath) {
+        Document doc = readXMLDocument(filePath);
+
+        Pair<Integer, Integer> mapSize = getMapSize(doc);
+
+        List<Tileset> tilesets = generateTilesetList(doc);
+
+        return generateTextureTileMap();
+    }
+
+    private static TextureMap generateTextureTileMap() {
+        // TODO: Implement
+        throw new NotImplementedException();
+    }
+
+    private static List<Tileset> generateTilesetList(Document doc) {
+        ArrayList<Tileset> tilesets = new ArrayList<>();
+
+        NodeList tilesetNodeList = doc.getElementsByTagName("tileset");
+        for (int i = 0; i < tilesetNodeList.getLength(); i++) {
+
+            String name, imgSource, firstGid, tileWidth, tileHeight, tileCount, columns;
+            NamedNodeMap attributes = tilesetNodeList.item(i).getAttributes();
+            for (int j = 0; j < attributes.getLength(); j++) {
+                switch (attributes.item(j).getNodeName()) {
+                    case "firstgid":
+                        firstGid = attributes.item(i).getNodeValue();
+                    case "name":
+                        name = attributes.item(i).getNodeValue();
+                    case "tilewidth":
+                        tileWidth = attributes.item(i).getNodeValue();
+                    case "tileheight":
+                        tileHeight = attributes.item(i).getNodeValue();
+                    case "tilecount":
+                        tileCount = attributes.item(i).getNodeValue();
+                    case "columns":
+                        columns = attributes.item(i).getNodeValue();
+                    default:
+                        System.out.println("Unknown tileset attribute found while parsing: " + attributes.item(i).getNodeValue());
+                }
+            }
+            NamedNodeMap childAttributes = tilesetNodeList.item(i).getFirstChild().getAttributes();
+            for (int j = 0; j < childAttributes.getLength(); j++) {
+                if (childAttributes.item(j).getNodeName().equals("source")) {
+                    imgSource = childAttributes.item(j).getNodeValue();
+                }
+            }
+            tilesets.add(new Tileset(imgSource, name, firstGid, tileWidth, tileHeight, tileCount, columns));
+        }
+        return tilesets;
+    }
+
+    private static Pair<Integer, Integer> getMapSize(Document doc) {
+        Node mapNode = doc.getElementsByTagName("map").item(0);
+        int width = Integer.parseInt(mapNode.getAttributes().getNamedItem("width").getNodeValue());
+        int height = Integer.parseInt(mapNode.getAttributes().getNamedItem("height").getNodeValue());
+        return new Pair<>(width, height);
+    }
+    private static Document readXMLDocument(String filePath) {
+        // Most of the information writing this parser was gotten from this tutorial:
+        // https://mkyong.com/java/how-to-read-xml-file-in-java-dom-parser/
+
+        // Create a new document builder factory.
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        try {
+            // Safely parse XML file.
+            dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            Document doc = db.parse(new File(filePath));
+
+            // Normalizes the document tree for better parsing.
+            // More info:
+            // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+            doc.getDocumentElement().normalize();
+
+            return doc;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
