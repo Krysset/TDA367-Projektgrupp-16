44458dc2d42578120949d3b49197258f4fdd2e7f
Karl Kjellmer <karl.kjellmer@gmail.com>
2022-09-08 14:32:11 +0200

    simple changer of animations Co-authored-by: Supergamer1337 <felix.b.aronsson@gmail.com>

--- core/src/com/g16/feyrune/view/player/PlayerRenderer.java
+++ core/src/com/g16/feyrune/view/player/PlayerRenderer.java
@@ -11,6 +11,8 @@ public class PlayerRenderer {
     private final TextureRegion currentTexture; // This is just temporary, until we figure out the animation handling.
     private final Texture texture;
     private final TextureRegion[] walkRightAnimation;
+    private final TextureRegion[] walkLeftAnimation;
+    private TextureRegion[] currentAnimation;
 
     float stateTime = 0;
 
@@ -18,20 +20,26 @@ public class PlayerRenderer {
         this.texture = texture;
         currentTexture = new TextureRegion(texture, x, y, width, height);
         walkRightAnimation = AnimationUtils.getAnimationFrames(texture, 8, 6, 4, 16);
+        walkLeftAnimation = AnimationUtils.getAnimationFrames(texture, 8, 6, 4, 20);
+        currentAnimation = walkRightAnimation;
     }
 
     public void draw(Batch batch) {
         int screenHeight = Gdx.graphics.getHeight();
         int screenWidth = Gdx.graphics.getWidth();
         stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
-        Animation<TextureRegion> animation = animate(walkRightAnimation);
+        Animation<TextureRegion> animation = animate(currentAnimation);
         animation.setPlayMode(Animation.PlayMode.LOOP);
         batch.draw(animation.getKeyFrame(stateTime), screenWidth / 2f - (currentTexture.getRegionWidth() / 2f),
                 screenHeight / 2f - (currentTexture.getRegionHeight() / 2f));
     }
 
+    public void changeAnimation(boolean ringht){
+        currentAnimation = ringht ? walkRightAnimation : walkLeftAnimation;
+    }
+
     public Animation<TextureRegion> animate(TextureRegion[] animation){
-        return new Animation<TextureRegion>(0.15f, animation);
+        return new Animation<>(0.15f, animation);
     }
 
     public void dispose() {
