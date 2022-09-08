6511a3ea084cbb8ad6a3e397899d2dd2fbce25ba
x183 <palmoscar4@gmail.com>
2022-09-08 10:53:14 +0200

    Fixed the FancyLabel to actually print stuff

--- core/src/com/g16/feyrune/view/components/FancyLabel.java
+++ core/src/com/g16/feyrune/view/components/FancyLabel.java
@@ -5,33 +5,37 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.Align;
 
-public class FancyLabel extends Label {
+public class FancyLabel extends Actor {
 
     private String imageString;
     private Image image;
-    private LabelStyle style;
+    private Label.LabelStyle style;
+    private Label label;
+
+    private String text;
     public FancyLabel(Vector2 position, String text, String backgroundImage, int width, int height) {
-        this(text, new LabelStyle());
-        super.setSize(width, height);
-        super.setPosition(position.x, position.y);
-        super.setAlignment(Align.center);
         this.imageString = backgroundImage;
-        this.image = new Image(new TextureRegion(new Texture(backgroundImage)));
+        this.text = text;
+        style= new Label.LabelStyle();
+        style.font = new BitmapFont(Gdx.files.internal("fonts/superAwesomeFont.fnt"));
+        label = new Label(this.text, style);
+        image = new Image(new TextureRegion(new Texture("components/cool.png")));
+        image.setPosition(position.x,position.y);
+        image.setSize(width,height);
+        label.setPosition(position.x,position.y);
+        label.setSize(width,height);
 
     }
-    private FancyLabel(String text, LabelStyle style) {
-        super(text, style);
-        this.style=style;
-        this.style.font = new BitmapFont(Gdx.files.internal("fonts/ShareTechMono-Regular.tff"));
-    }
+
 
     @Override
     public void draw(com.badlogic.gdx.graphics.g2d.Batch batch, float parentAlpha) {
-        super.draw(batch, parentAlpha);
         image.draw(batch, parentAlpha);
+        label.draw(batch,parentAlpha);
     }
 }
