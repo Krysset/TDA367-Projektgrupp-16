ef0a13dce70927f2c31442e7ee2d780c533edad3
Karl Kjellmer <karl.kjellmer@gmail.com>
2022-09-08 13:54:44 +0200

    AnimationUtils added, for getting animation frames from sprite sheet
    Co-authored-by: Supergamer1337 <felix.b.aronsson@gmail.com>

---
+++ core/src/com/g16/feyrune/view/utils/AnimationUtils.java
@@ -0,0 +1,33 @@
+package com.g16.feyrune.view.utils;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class AnimationUtils {
+    public static TextureRegion[] getAnimationFrames(Texture texture, int sizeX, int sizeY, int numOfFrames, int animationPosition){
+
+        // Use the split utility method to create a 2D array of TextureRegions. This is
+        // possible because this sprite sheet contains frames of equal size and they are
+        // all aligned.
+        TextureRegion[][] tmp = TextureRegion.split(texture,
+                texture.getWidth() / sizeX,
+                texture.getHeight() / sizeY);
+
+        // Place the regions into a 1D array in the correct order, starting from the top
+        // left, going across first. The Animation constructor requires a 1D array.
+        TextureRegion[] frames = new TextureRegion[sizeX * sizeY];
+        int index = 0;
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeY; j++) {
+                frames[index++] = tmp[i][j];
+            }
+        }
+
+        TextureRegion[] animationFrames = new TextureRegion[numOfFrames];
+        for(int i = 0; i < numOfFrames; i++){
+            animationFrames[i] = frames[animationPosition + i];
+        }
+
+        return animationFrames;
+    }
+}
