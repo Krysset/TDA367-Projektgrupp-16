97da3ee9c4411b9685da4c6853ce93e9a7e3f028
x183 <palmoscar4@gmail.com>
2022-09-27 08:06:13 +0200

    Finished implementing and connecting the base version of attackFactory

--- core/src/main/java/com/g16/feyrune/model/action/attackFactory.java
+++ core/src/main/java/com/g16/feyrune/model/action/attackFactory.java
@@ -1,5 +1,26 @@
 package com.g16.feyrune.model.action;
 
 public class attackFactory {
-    public static
+    public static BaseAttack createAttack(String name, int accuracy, int power) {
+        return new BaseAttack(accuracy, power, name);
+    }
+
+    public static BaseAttack[] createAttackList(String[] names) {
+        BaseAttack[] attacks = new BaseAttack[names.length];
+        for (int i = 0; i < names.length; i++) {
+            attacks[i] = createAttackByName(names[i]);
+        }
+        return attacks;
+    }
+
+    private static BaseAttack createAttackByName(String name) {
+        switch (name) {
+            case "SuperAwesomeBaseAttack":
+                return new BaseAttack(100, 100, name);
+            case "SuperBadStandardAttack":
+                return new BaseAttack(50, 50, name);
+            default:
+                return new BaseAttack(50, 50, name);
+        }
+    }
 }
--- core/src/main/java/com/g16/feyrune/model/creature/CreatureFactory.java
+++ core/src/main/java/com/g16/feyrune/model/creature/CreatureFactory.java
@@ -3,62 +3,52 @@ package com.g16.feyrune.model.creature;
 import com.g16.feyrune.Util.Random;
 import com.g16.feyrune.interfaces.ICreature;
 import com.g16.feyrune.model.action.BaseAttack;
+import com.g16.feyrune.model.action.attackFactory;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import com.g16.feyrune.Util.Pair;
 
 public class CreatureFactory {
     /**
-     *
      * @return a new Object of type ICreature
      */
     public static ICreature createCreature(String attackNames[], int health, int power, int speed, int evasion) {
-        BaseAttack[] baseAttackList= new BaseAttack[attackNames.length];
-        for(int i = 0; i < attackNames.length; i++){
-            baseAttackList[i] = createAttackByName(attackNames[i]);
-        }
-        return new BaseCreature(100,75,50,33, baseAttackList);
+        BaseAttack[] baseAttackList = new BaseAttack[attackNames.length];
+        baseAttackList = attackFactory.createAttackList(attackNames);
+        return new BaseCreature(100, 75, 50, 33, baseAttackList);
     }
 
-    public static ICreature[] createCreatureList(Pair<String, Integer>[] monsterList, int wantedAmount){
+    public static ICreature[] createCreatureList(Pair<String, Integer>[] monsterList, int wantedAmount) {
         ICreature[] iCreatures = new ICreature[wantedAmount];
-        for(int i = 0; i < wantedAmount; i++){
+        for (int i = 0; i < wantedAmount; i++) {
             String nextMonster = calculateCreatureToUse(monsterList);
         }
         return iCreatures;
     }
-    private static String calculateCreatureToUse(Pair<String, Integer>[] monsterList){
+
+    private static String calculateCreatureToUse(Pair<String, Integer>[] monsterList) {
         int monsterCards = 0;
-        for(Pair<String, Integer> monster : monsterList){
+        for (Pair<String, Integer> monster : monsterList) {
             monsterCards += monster.getSnd();
         }
-        int random = Random.randomInt( monsterCards);
-        for(Pair<String, Integer> monster : monsterList){
+        int random = Random.randomInt(monsterCards);
+        for (Pair<String, Integer> monster : monsterList) {
             random -= monster.getSnd();
-            if(random <= 0){
+            if (random <= 0) {
                 return monster.getFst();
             }
         }
         return "default";
     }
-    private static ICreature createCreatureByName(String name){
-        switch (name){
+
+    private static ICreature createCreatureByName(String name) {
+        switch (name) {
             case "SuperAwesomeBaseMonster":
-                return createCreature(new String[] {"SuperAwesomeBaseAttack"}, 100, 75, 50, 33);
+                return createCreature(new String[]{"SuperAwesomeBaseAttack"}, 100, 75, 50, 33);
             case "SuperBadBaseMonster":
-                return createCreature(new String[] {"SuperBadStandardAttack"}, 50, 15, 10, 85);
+                return createCreature(new String[]{"SuperBadStandardAttack"}, 50, 15, 10, 85);
             default:
                 throw new NotImplementedException();
         }
     }
 
-    private static BaseAttack createAttackByName(String name){
-        switch (name){
-            case "SuperAwesomeBaseAttack":
-                return new BaseAttack(100,100,"SuperAwesomeBaseAttack");
-            case "SuperBadStandardAttack":
-                return new BaseAttack(50,50,"SuperBadStandardAttack");
-            default:
-                throw new NotImplementedException();
-        }
-    }
 }
