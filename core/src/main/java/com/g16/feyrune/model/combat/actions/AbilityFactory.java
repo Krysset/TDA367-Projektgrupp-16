a02510deae21d30e4c02d538380b327068f33232
x183 <palmoscar4@gmail.com>
2022-09-27 11:05:18 +0200

    Wrong name in UML and functions Co-authored-by: Lukas Åkefeldt <91898187+lutches@users.noreply.github.com>

--- core/src/main/java/com/g16/feyrune/model/combat/actions/AbilityFactory.java
+++ core/src/main/java/com/g16/feyrune/model/combat/actions/AbilityFactory.java
@@ -3,19 +3,20 @@ package com.g16.feyrune.model.combat.actions;
 import com.g16.feyrune.model.combat.actions.abilities.BaseAbility;
 
 public class AbilityFactory {
-    public static BaseAbility createAttack(String name, int accuracy, int power) {
+    public static BaseAbility createAbility(String name, int accuracy, int power) {
         return new BaseAbility(accuracy, power, name);
     }
 
-    public static BaseAbility[] createAttackList(String[] names) {
+    public static BaseAbility[] createAbilityList(String[] names) {
         BaseAbility[] attacks = new BaseAbility[names.length];
         for (int i = 0; i < names.length; i++) {
-            attacks[i] = createAttackByName(names[i]);
+            
+            attacks[i] = createAbilityByName(names[i]);
         }
         return attacks;
     }
 
-    private static BaseAbility createAttackByName(String name) {
+    private static BaseAbility createAbilityByName(String name) {
         switch (name) {
             case "SuperAwesomeBaseAbility":
                 return new BaseAbility(100, 100, name);
