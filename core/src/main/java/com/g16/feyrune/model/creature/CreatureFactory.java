52aa05bcc3c1f95fad442899b4ac6c280fdbc009
låke <91898187+lutches@users.noreply.github.com>
2022-09-27 10:07:17 +0200

    Wrote Integer instead of String! Whoopsie

--- core/src/main/java/com/g16/feyrune/model/creature/CreatureFactory.java
+++ core/src/main/java/com/g16/feyrune/model/creature/CreatureFactory.java
@@ -1,54 +1,53 @@
 package com.g16.feyrune.model.creature;
 
 import com.g16.feyrune.Util.Random;
+import com.g16.feyrune.interfaces.IAbility;
 import com.g16.feyrune.interfaces.ICreature;
-import com.g16.feyrune.model.action.BaseAttack;
-import com.g16.feyrune.model.action.attackFactory;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import com.g16.feyrune.model.combat.actions.AbilityFactory;
+import com.g16.feyrune.model.combat.actions.abilities.BaseAbility;
 import com.g16.feyrune.Util.Pair;
 
 public class CreatureFactory {
     /**
+     *
      * @return a new Object of type ICreature
      */
-    public static ICreature createCreature(String attackNames[], int health, int power, int speed, int evasion) {
-        BaseAttack[] baseAttackList = new BaseAttack[attackNames.length];
-        baseAttackList = attackFactory.createAttackList(attackNames);
-        return new BaseCreature(100, 75, 50, 33, baseAttackList);
+    public static ICreature createCreature(String[] abilities, double health, int power, int speed, int evasion) {
+        BaseAbility[] baseAbilityList = AbilityFactory.createAttackList(abilities);
+        return new BaseCreature(health,power, speed, evasion, baseAbilityList);
     }
-
-    public static ICreature[] createCreatureList(Pair<String, Integer>[] monsterList, int wantedAmount) {
+    public static ICreature[] createCreatureList(Pair<String, Integer>[] monsterList, int wantedAmount){
         ICreature[] iCreatures = new ICreature[wantedAmount];
-        for (int i = 0; i < wantedAmount; i++) {
-            String nextMonster = calculateCreatureToUse(monsterList);
+        for(int i = 0; i < wantedAmount; i++){
+            String name = creatureToUse(monsterList);
+            iCreatures[i] = createCreatureByName(name);
         }
         return iCreatures;
     }
 
-    private static String calculateCreatureToUse(Pair<String, Integer>[] monsterList) {
-        int monsterCards = 0;
-        for (Pair<String, Integer> monster : monsterList) {
-            monsterCards += monster.getSnd();
+    private static String creatureToUse(Pair<String, Integer>[] monsterList){
+        int total = 0;
+        for(int i = 0; i < monsterList.length; i++){
+            total += monsterList[i].getSnd();
         }
-        int random = Random.randomInt(monsterCards);
-        for (Pair<String, Integer> monster : monsterList) {
-            random -= monster.getSnd();
-            if (random <= 0) {
-                return monster.getFst();
+        int random = Random.randomInt(total);
+        int current = 0;
+        for(int i = 0; i < monsterList.length; i++){
+            current += monsterList[i].getSnd();
+            if(random <= current){
+                return monsterList[i].getFst();
             }
         }
-        return "default";
+        return monsterList[0].getFst();
     }
-
-    private static ICreature createCreatureByName(String name) {
-        switch (name) {
+    private static ICreature createCreatureByName(String name){
+        switch (name){
             case "SuperAwesomeBaseMonster":
-                return createCreature(new String[]{"SuperAwesomeBaseAttack"}, 100, 75, 50, 33);
+                return createCreature(new String[]{"SuperAwesomeBaseAbility"}, 100, 100, 100, 100);
             case "SuperBadBaseMonster":
-                return createCreature(new String[]{"SuperBadStandardAttack"}, 50, 15, 10, 85);
+                return createCreature(new String[]{"SuperBadBaseAbility"}, 100, 100, 100, 100);
             default:
-                throw new NotImplementedException();
+                return createCreature(new String[]{"SuperBasicBaseAbility"}, 100, 100, 100, 100);
         }
     }
-
 }
