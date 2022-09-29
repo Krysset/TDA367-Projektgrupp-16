c5ddc39a5b5858d017acdacf3b8509648cc9019d
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2022-09-29 14:16:22 +0200

    Parses transporters from map

---
+++ core/src/main/java/com/g16/feyrune/model/overworld/map/Transporter.java
@@ -0,0 +1,35 @@
+package com.g16.feyrune.model.overworld.map;
+
+import java.awt.*;
+
+public class Transporter {
+    private final String mapAssetPath;
+    private final Point from;
+    private final Point to;
+
+    protected Transporter(String mapAssetPath, Point from, Point to) {
+        this.mapAssetPath = mapAssetPath;
+        this.from = from;
+        this.to = to;
+    }
+
+    public int getFromX() {
+        return from.x;
+    }
+
+    public int getFromY() {
+        return from.y;
+    }
+
+    public Point getTransitionFrom() {
+        return new Point(from);
+    }
+
+    public Point getTransitionTo() {
+        return new Point(to);
+    }
+
+    public String getMapAssetPath() {
+        return mapAssetPath;
+    }
+}
