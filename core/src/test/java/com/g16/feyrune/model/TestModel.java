fa580145374695b4208d4d17a1b93b838eb7d28f
Krysset <maxhagman@hotmail.se>
2022-10-18 14:43:27 +0200

    Created test class

---
+++ core/src/test/java/com/g16/feyrune/model/TestModel.java
@@ -0,0 +1,25 @@
+package com.g16.feyrune.model;
+
+import org.junit.Test;
+
+public class TestModel {
+    @Test
+    public void update() {
+
+    }
+
+    @Test
+    public void testRegisterNewObserver() {
+
+    }
+
+    @Test
+    public void testChangeState() {
+
+    }
+
+    @Test
+    public void testObserverUpdate() {
+
+    }
+}
