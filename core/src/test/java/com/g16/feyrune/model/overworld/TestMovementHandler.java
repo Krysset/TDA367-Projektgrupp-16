f8abe170b230e46e01c3f767e8abe38b343310a6
Karl Kjellmer <karl.kjellmer@gmail.com>
2022-10-18 18:14:48 +0200

    fixed some testes

--- core/src/test/java/com/g16/feyrune/model/overworld/TestMovementHandler.java
+++ core/src/test/java/com/g16/feyrune/model/overworld/TestMovementHandler.java
@@ -10,39 +10,78 @@ import static org.junit.Assert.assertEquals;
 public class TestMovementHandler {
     @Test
     public void testIncreaseXDirection() {
-        Point expected = new Point();
+        Point expected = new Point(2,1);
         MovementHandler mh = new MovementHandler();
         mh.increaseXDirection();
-        Point actual = mh.calculateMovement(new Point(1,1), new MapManager("assets/maps/plains1.tmx"));
+        Point actual = mh.calculateMovement(new Point(1,1), new MapManager("../assets/maps/plains1.tmx"));
         assertEquals(expected.x, actual.x);
         assertEquals(expected.y, actual.y);
     }
     @Test
     public void testDecreaseXDirection() {
+        Point expected = new Point(0,1);
         MovementHandler mh = new MovementHandler();
+        mh.decreaseXDirection();
+        Point actual = mh.calculateMovement(new Point(1,1), new MapManager("../assets/maps/plains1.tmx"));
+        assertEquals(expected.x, actual.x);
+        assertEquals(expected.y, actual.y);
     }
     @Test
     public void testIncreaseYDirection() {
-
+        Point expected = new Point(1,2);
+        MovementHandler mh = new MovementHandler();
+        mh.increaseYDirection();
+        Point actual = mh.calculateMovement(new Point(1,1), new MapManager("../assets/maps/plains1.tmx"));
+        assertEquals(expected.x, actual.x);
+        assertEquals(expected.y, actual.y);
     }
     @Test
     public void testDecreaseYDirection() {
-
+        Point expected = new Point(1,0);
+        MovementHandler mh = new MovementHandler();
+        mh.decreaseYDirection();
+        Point actual = mh.calculateMovement(new Point(1,1), new MapManager("../assets/maps/plains1.tmx"));
+        assertEquals(expected.x, actual.x);
+        assertEquals(expected.y, actual.y);
     }
     @Test
     public void testResetDirectionX() {
-
+        Point expected = new Point(1,1);
+        MovementHandler mh = new MovementHandler();
+        mh.increaseXDirection();
+        mh.resetXDirection();
+        Point actual = mh.calculateMovement(new Point(1,1), new MapManager("../assets/maps/plains1.tmx"));
+        assertEquals(expected.x, actual.x);
+        assertEquals(expected.y, actual.y);
     }
     @Test
     public void testResetDirectionY() {
-
+        Point expected = new Point(1,1);
+        MovementHandler mh = new MovementHandler();
+        mh.increaseYDirection();
+        mh.resetYDirection();
+        Point actual = mh.calculateMovement(new Point(1,1), new MapManager("../assets/maps/plains1.tmx"));
+        assertEquals(expected.x, actual.x);
+        assertEquals(expected.y, actual.y);
     }
     @Test
     public void testResetMovement() {
-
+        Point expected = new Point(1,1);
+        MovementHandler mh = new MovementHandler();
+        mh.increaseYDirection();
+        mh.increaseXDirection();
+        mh.resetMovement();
+        Point actual = mh.calculateMovement(new Point(1,1), new MapManager("../assets/maps/plains1.tmx"));
+        assertEquals(expected.x, actual.x);
+        assertEquals(expected.y, actual.y);
     }
     @Test
     public void testCalculateMovement() {
-
+        Point expected = new Point(1,1);
+        MovementHandler mh = new MovementHandler();
+        mh.resetMovement();
+        Point actual = mh.calculateMovement(new Point(1,1), new MapManager("../assets/maps/plains1.tmx"));
+        assertEquals(expected.x, actual.x);
+        assertEquals(expected.y, actual.y);
     }
 }
